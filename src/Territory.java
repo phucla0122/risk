f59908467a0b35a8ac84003d537c3c926a66a0c4
jacobbschmidt <schmidtjacob101@gmail.com>
2020-12-07 18:41:24 -0500

    removed useless method i added into territory

--- src/Territory.java
+++ src/Territory.java
@@ -135,14 +135,6 @@ public class Territory {
         return output;
     }
 
-    public boolean hasAdjacentEnemy(Game gameState) {
-        Optional<Territory> temp;
-        for (String toCheck : listOfAdjacents){
-            temp = gameState.findTerritory(toCheck);
-            if (temp.isPresent() && temp.get().getOwner() != owner) return true;
-        }
-        return false;
-    }
 
     @Override
     public String toString() {
